f52aef7adddec488e8dbf131cd8bb5fb0122d9c3
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2023-01-12 19:49:09 +0100

    initial commit 🚀

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '$lib/app.css';
+</script>
+
+<main>
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<div class="flex h-screen place-content-center items-center p-5">
+	<h1 class="text-4xl">Sveltekit ➕ Prettier ➕ TailwindCSS</h1>
+</div>
